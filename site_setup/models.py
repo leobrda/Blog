c87732ec2ea9cc18db0ba7ef584306d2d4c2f121
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-10-11 11:27:14 -0300

    Field validators, sobrescrevendo save do Model e redimensionando imagens

--- site_setup/models.py
+++ site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validators import validate_png
+from utils.images import resize_image
 
 
 # Create your models here.
@@ -38,7 +40,16 @@ class SiteSetup(models.Model):
     favicon = models.ImageField(
         upload_to='assets/favicon/%Y/%m',
         blank=True, default='',
+        validators=[validate_png],
     )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+
+        favicon_changed = False
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
