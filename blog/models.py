9998ace2e93f06232969c2a3780d9f2eded537fe
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-10-14 15:37:48 -0300

    get_absolute_url no models.py do app blog + criando campo readonly na admin do Django

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from utils.rands import slugify_new
 from utils.images import resize_image
 from django_summernote.models import AbstractAttachment
+from django.urls import reverse
 
 
 class PostAttachment(AbstractAttachment):
@@ -112,6 +113,12 @@ class Post(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='post_updated_by',)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,)
     tags = models.ManyToManyField(Tag, blank=True, default='')
+
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+
+        return reverse('blog:post', args=(self.slug,))
 
     def save(self, *args, **kwargs):
         if not self.slug:
