ffe85316c4396296c4a74dc3111f0b1863cb0707
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-10-14 14:44:33 -0300

    Filtrando e ordenando os posts enviados para o template da view def index() em views.py do app blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from blog.models import Post
+
+PER_PAGE = 9
 
 
 def index(request):
+    posts = Post.objects.filter(is_published=True).order_by('-pk')
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'blog/pages/index.html',
+
+        {
+            'page_obj': page_obj,
+        }
     )
 
 
